ff13713a0602ae350b48fc2893d2917abd2e012a
ReD <danielrosiqueegea@gmail.com>
2024-10-13 20:06:34 +0200

    s

--- functionality.js
+++ functionality.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $("p").hide();
-      $("button").click(function(){
-        $("p").show();
-      })
-    });
-  });
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $("p").hide();
+//       $("button").click(function(){
+//         $("p").show();
+//       })
+//     });
+//   });
