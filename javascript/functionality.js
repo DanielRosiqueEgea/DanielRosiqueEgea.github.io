5f578a5aad628d46fc03a7fca3eef7c936a4bb77
Daniel Rosique Egea <danielrosiqueegea@gmail.com>
2024-12-03 15:01:33 +0100

    Limpiada la pagina#

--- javascript/functionality.js
+++ javascript/functionality.js
@@ -16,7 +16,7 @@ $(function(){
   console.log("Se va crear la pagina");
   $("#includeNavbar").load("snippets/navbar.html"); 
   $("#includePageContent").load("snippets/pageContent.html");
-  $("#includeBandSection").load("snippets/bandSection.html");
+  $("#includeIntroduciton").load("snippets/introduction.html");
   $("#includeTourSection").load("snippets/tourSection.html");
   $("#includeTicketModal").load("snippets/ticketModal.html");
   $("#includeContactSection").load("snippets/contactSection.html");
